185224e5721e181bb8a7311bee91161b20d9f4b3
Prajwal S <prajwalplays@gmail.com>
2024-01-16 19:20:05 +0530

    Update mesgList.jsx
    - added a navigation link
    - the 'to' prop is set to /messages/${mesg.id}, which likely creates a dynamic link to the message detail page with the corresponding id
    - made a few css changes

--- src/components/mesgList.jsx
+++ src/components/mesgList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //using props -> Props are arguments passed into React components. (props stands for properties.)
 //Props are passed to components via HTML attributes.
@@ -7,14 +8,27 @@ const mesgList = ({ messages }) => {
   // const messages = props.messages;
   //   console.log(props, messages)
 
+  const emojis = ["✉️", "😭", "💌", "💝", "🌃", "🥹"];
+
+  const getRandomEmoji = () => {
+    const randomIndex = Math.floor(Math.random() * emojis.length);
+    return emojis[randomIndex];
+  };
+
   return (
-    <div className="flex flex-wrap justify-center h-[80%] gap-8 p-7 w-full xl:w-[60%] mx-auto">
+    <div className='flex flex-wrap justify-center h-[80%] gap-8 p-7 w-full xl:w-[60%] mx-auto pt-5'>
       {messages.map((mesg) => (
-        <div key={mesg.id} className="bg-[#2e2e2e] p-8 h-[50vh] w-[40vh] rounded-lg shadow-lgtext-center">
-            <h1 className="text-[#dfdfdf] text-4xl font-serif font-[500] selection:bg-fuchsia-300 selection:text-fuchsia-900 flex justify-start">To: {mesg.name}</h1>
-          <p className="text-[#dfdfdf] mt-4 text-2xl text-start font-serif selection:bg-fuchsia-300 selection:text-fuchsia-900">{mesg.mesg}</p>
-          {/* <button onClick={() => handleDelete(mesg.id)} className="bg-white">delete</button> */}
-        </div>
+        <Link key={mesg.id} to={`/messages/${mesg.id}`}>
+          <div key={mesg.id} className='bg-[#2e2e2e] p-8 h-[50vh] w-[40vh] rounded-md shadow-lg text-center cursor-pointer'>
+            <h1 className='text-[#dfdfdf] text-4xl font-serif font-[500] flex justify-start'>
+              To: {mesg.name}
+              <span className='cursor-pointer pl-2 text-2xl pt-1'>✉️</span>
+            </h1>
+            <p className='text-[#dfdfdf] mt-4 text-2xl text-start font-serif  whitespace-normal'>{mesg.mesg}</p>
+
+            {/* <button onClick={() => handleDelete(mesg.id)} className="bg-white">delete</button> */}
+          </div>
+        </Link>
       ))}
     </div>
   );
