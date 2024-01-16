7934acccff1a384297e6292456f6903465bff784
Prajwal S <prajwalplays@gmail.com>
2024-01-16 19:09:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
-import Navbar from "./components/navbar";
-import Home from "./components/home";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 // import Search from "./components/search";
-import Create from "./components/create";
+import Create from "./components/Create";
+import MesgDetails from "./components/MesgDetails";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Navbar />
         {/* <Search /> */}
         <Switch>
           {/* adding exact because react thinks '/create' as '/' as it matches the starting string  */}
-          <Route exact path="/">
+          <Route exact path='/'>
             <Home />
           </Route>
-          <Route path="/create">
+          <Route path='/create'>
             <Create />
+          </Route>
+          <Route path='/messages/:id'>
+            <MesgDetails />
           </Route>
         </Switch>
       </div>
