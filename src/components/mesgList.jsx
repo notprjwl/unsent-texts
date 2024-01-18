26f87d12a6ea07828869302ff1912f64fa413689
Prajwal S <prajwalplays@gmail.com>
2024-01-19 00:40:23 +0530

    Update mesgList.jsx
    update design

--- src/components/mesgList.jsx
+++ src/components/mesgList.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
+
 
 //using props -> Props are arguments passed into React components. (props stands for properties.)
 //Props are passed to components via HTML attributes.
 
-const mesgList = ({ messages }) => {
+const MesgList = ({ messages }) => {
   // const messages = props.messages;
   //   console.log(props, messages)
 
@@ -15,16 +16,17 @@ const mesgList = ({ messages }) => {
     return emojis[randomIndex];
   };
 
+
   return (
     <div className='flex flex-wrap justify-center h-[80%] gap-8 p-7 w-full xl:w-[60%] mx-auto pt-5'>
       {messages.map((mesg) => (
         <Link key={mesg.id} to={`/messages/${mesg.id}`}>
-          <div key={mesg.id} className='bg-[#2e2e2e] p-8 h-[50vh] w-[40vh] rounded-md shadow-lg text-center cursor-pointer'>
-            <h1 className='text-[#dfdfdf] text-4xl font-serif font-[500] flex justify-start'>
+          <div key={mesg.id} className="message-card bg-[#10100e] p-8 text-[#ffffe3] h-[50vh] w-[40vh] rounded-md shadow-lg text-center cursor-pointer  hover:bg-[#ffffe3] hover:text-[#10100e] border border-[#30302b]">
+            <h1 className='text-4xl font-serif font-[500] flex justify-start'>
               To: {mesg.name}
-              <span className='cursor-pointer pl-2 text-2xl pt-1'>✉️</span>
+              <span className='cursor-pointer pl-2 text-2xl pt-1'></span>
             </h1>
-            <p className='text-[#dfdfdf] mt-4 text-2xl text-start font-serif  whitespace-normal'>{mesg.mesg}</p>
+            <p className='mt-4 text-2xl text-start font-serif whitespace-normal'>{mesg.mesg}</p>
 
             {/* <button onClick={() => handleDelete(mesg.id)} className="bg-white">delete</button> */}
           </div>
@@ -34,4 +36,4 @@ const mesgList = ({ messages }) => {
   );
 };
 
-export default mesgList;
+export default MesgList;
