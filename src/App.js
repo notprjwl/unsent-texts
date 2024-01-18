15c5f4f434164ef7deab20979abddf8c8e461955
Prajwal S <prajwalplays@gmail.com>
2024-01-19 00:39:03 +0530

    Update App.js
    update design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Main from "./components/Main";
 // import Search from "./components/search";
 import Create from "./components/Create";
 import MesgDetails from "./components/MesgDetails";
@@ -16,6 +17,9 @@ function App() {
           {/* adding exact because react thinks '/create' as '/' as it matches the starting string  */}
           <Route exact path='/'>
             <Home />
+          </Route>
+          <Route exact path='/messages'>
+            <Main />
           </Route>
           <Route path='/create'>
             <Create />
