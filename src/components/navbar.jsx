3cb280dc99ceedbe7ed01b076c5eb2e0fb2e2da1
Prajwal S <prajwalplays@gmail.com>
2024-01-19 00:40:27 +0530

    Update navbar.jsx
    update design

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="bg-[#191919] flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold text-[#bdbdbd] font-pt center p-4 hover:text-white">
+    <div className='bg-[#10100e] flex justify-between items-center border-box border-b border-[#30302b]'>
+      <Link to='/' className='text-2xl font-bold text-[#ffffe3] font-pt center p-4 hover:e6e6cc tracking-tighter'>
         Unsent Texts.
       </Link>
-      <div className="flex items-center font-mono font-bold hover:text-white">
-        <Link to="/" className="text-[#bdbdbd] p-5 hover:text-white ">
+      <div className='flex items-center font-mono font-bold text-[#ffffe3] '>
+        {/* <Link to="/" className="text-[#bdbdbd] p-5 hover:text-white ">
           Home
+        </Link> */}
+        <Link to='/messages' className='p-5 border-x border-[#30302b] hover:bg-[#ffffe3] hover:text-[#10100e] '>
+          Archive
         </Link>
-        <Link to="/create" className="text-[#bdbdbd] whitespace-nowrap p-5 hover:text-white ">
+        <Link to='/create' className='whitespace-nowrap p-5 hover:bg-[#ffffe3] hover:text-[#10100e]'>
           New Message
         </Link>
       </div>
