e0b934f58b117fbc515bbafb6e87020b4839107b
Prajwal S <prajwalplays@gmail.com>
2024-01-19 20:10:42 +0530

    add search functionality
    - modified the search component to handle the input value directly using a controlled input approach
    - ensured that the input is trimmed before calling 'onSearch' prop
    - no search button is involved allowing dynamic filtering as user types

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { useState } from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
-const Search = () => {
+const Search = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (e) => {
+    const input = e.target.value;
+    setSearchQuery(input);
+    onSearch(input.trim() !== "" ? input.trim() : null);
+  };
+
   return (
-    <div className="text-center">
-      <input type="text" placeholder="search by name" className="rounded-md p-2 mt-4 bg-[#bdbdbd] text-[#191919] font-mono placeholder-[#191919d5] decoration-0 selection:bg-[#191919] selection:text-[#bdbdbd]" size={30} />
-      {/* <button type='submit' className='text-[#bdbdbd] rounded-md'>search</button> */}
+    <div className='flex justify-center items-center mt-4'>
+      <div className='relative max-w-100'>
+        <input type='text' placeholder='search by name' className='rounded-lg p-2 pr-10 text-background font-mono placeholder-background decoration-0 selection:bg-background selection:text-text caret-background bg-text border border-text focus:outline-none  w-full focus:ring-2 ring-shadow md:w-auto' size={30} value={searchQuery} onChange={handleSearch} />
+        <button className='absolute right-0 top-0 font-mono bg-text text-background rounded-lg p-2 text-center justify-center mt-[3px] items-center mr-1 cursor-default'>
+          <MagnifyingGlassIcon className='h-5' />
+        </button>
+      </div>
     </div>
   );
 };
