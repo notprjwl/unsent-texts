fedddb116a0535e211f2d2abdad5d2912f60e6b1
Prajwal S <prajwalplays@gmail.com>
2024-01-19 20:03:10 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <div>
     <main className="container mx-auto mt-[30vh]">
-      <h1 className="text-[#ffffe3] font-playfair font-black italic text-8xl tracking-tighter text-center">
+      <h1 className="text-text font-playfair font-black italic text-8xl tracking-tighter text-center">
         Unsent Texts.
       </h1>
-      <h1 className="text-[#ffffe3] mt-5 mx-auto whitespace-pre-wrap">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+      <h1 className="text-text mt-5 mx-auto whitespace-pre-wrap">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
     </main>
     </div>
   );
