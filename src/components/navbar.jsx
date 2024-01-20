636da576a3b7da450515f658db4a8c19bd7bf5d8
Prajwal S <prajwalplays@gmail.com>
2024-01-20 21:51:42 +0530

    Update navbar.jsx
    - added navbar animation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  });
+
   return (
-    <div className='bg-background flex justify-between items-center border-box border-b border-outline'>
+    <div className={`transition-all duration-500 ease-in-out transform ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0'} bg-background flex justify-between items-center border-box border-b border-outline`}>
       <Link to='/' className='text-2xl font-bold text-text font-pt center p-4 hover:e6e6cc tracking-tighter'>
         Unsent Texts.
       </Link>
+      {/* <button className="">
+        <a href=''>
+          <PlusCircleIcon className='text-text h-5' />
+        </a>
+      </button> */}
+
       <div className='flex items-center font-mono font-bold text-text '>
         {/* <Link to="/" className="text-[#bdbdbd] p-5 hover:text-white ">
           Home
